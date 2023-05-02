53f917f399b508543085309460634f1f2256b3f4
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2023-05-02 06:27:27 +0500

    ali check all function

---
+++ Test.py
@@ -0,0 +1,12 @@
+from Addition import add
+from Subtraction import subtract
+from Multiplication import multiply
+
+if __name__=="__main__":
+    a = 6
+    b = 4
+
+
+print(add(a,b))
+print(subtract(a,b))
+print(multiply(a,b))
